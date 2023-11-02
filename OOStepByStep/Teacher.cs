cef55258106030d00d8b1f7243af8f3b53f2a6c4
Han Xudong <xhan14@slb.com>
2023-11-02 19:10:24 +0800

    feat: improve function 'IntroduceOneself' by showing more information when the person is a teacher with classId

--- OOStepByStep/Teacher.cs
+++ OOStepByStep/Teacher.cs
@@ -8,13 +8,31 @@ namespace OOStepByStep
 {
     public class Teacher : Person
     {
+        private int classId;
         public Teacher(string name, int age) : base(name, age)
         {
+            classId = 0;
+        }
+
+        public int ClassId
+        {
+            get { return classId; }
+            set { classId = value; }
         }
 
         public override string IntroduceOneself()
         {
-            return base.IntroduceOneself() + " I'm a teacher.";
+            string introduction = base.IntroduceOneself();
+            if (classId == 0)
+            {
+                introduction += " I'm a teacher.";
+            }
+            else
+            {
+                introduction += $" I'm a teacher of class {classId}.";
+            }
+
+            return introduction;
         }
     }
 }
--- OOStepByStepTest/PersonWithClassTest.cs
+++ OOStepByStepTest/PersonWithClassTest.cs
@@ -24,5 +24,20 @@ namespace OOStepByStepTest
             // then
             Assert.Equal("My name is Kevin. I'm 28 years old. I'm a student of class 2.", result);
         }
+
+        [Fact]
+        public void Should_return_correct_introduction_sentence_when_introduce_oneself_given_a_teacher_with_class_number()
+        {
+            // given
+
+            Teacher teacher = new Teacher("Kevin", 28);
+            teacher.ClassId = 2;
+
+            // when
+            string result = teacher.IntroduceOneself();
+
+            // then
+            Assert.Equal("My name is Kevin. I'm 28 years old. I'm a teacher of class 2.", result);
+        }
     }
 }
