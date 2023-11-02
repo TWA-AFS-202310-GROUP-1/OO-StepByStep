7e3d4b7908a64c847c713ec799fa5b1138f43dd0
Han Xudong <xhan14@slb.com>
2023-11-02 19:47:17 +0800

    feat: add class 'ClassOfSchool' with parameter one teacher

---
+++ OOStepByStep/ClassOfSchool.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOStepByStep
+{
+    public class ClassOfSchool
+    {
+        private Teacher teacherOfClass;
+        private List<Student> students;
+
+        public Teacher TeacherOfClass
+        {
+            get
+            {
+                return teacherOfClass;
+            }
+            set
+            {
+                teacherOfClass = value;
+            }
+        }
+
+        public List<Student> Students
+        {
+            get
+            {
+                return students;
+            }
+        }
+
+        public Teacher ShowTeacher()
+        {
+            return teacherOfClass;
+        }
+
+        public void Join(Student newStudent)
+        {
+            return;
+        }
+    }
+}
---
+++ OOStepByStepTest/ClassTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace OOStepByStepTest
+{
+    using OOStepByStep;
+    using Xunit;
+
+    // tests for different kinds of person, including Person, Student and Teacher
+    public class ClassTest
+    {
+        [Fact]
+        public void Should_return_teacher_of_the_class_when_ShowTeacher_given_a_ClassOfSchool()
+        {
+            // given
+            ClassOfSchool class2 = new ClassOfSchool();
+            Teacher teacher = new Teacher("Bob", 16);
+            teacher.ClassId = 2;
+            class2.TeacherOfClass = teacher;
+
+            // when
+            string result = class2.ShowTeacher().IntroduceOneself();
+
+            // then
+            Assert.Equal("My name is Bob. I'm 16 years old. I'm a teacher of class 2.", result);
+        }
+    }
+}
