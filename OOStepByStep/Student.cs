06c6fefbd3b4204b8be1dc2dd9d79b516a59ed3a
Han Xudong <xhan14@slb.com>
2023-11-02 18:41:36 +0800

    feat: add class 'Student' with function 'IntroduceOneself' override

---
+++ OOStepByStep/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace OOStepByStep
+{
+    public class Student : Person
+    {
+        public Student(string name, int age) : base(name, age)
+        {
+        }
+
+        public override string IntroduceOneself()
+        {
+            return base.IntroduceOneself() + " I'm a student.";
+        }
+    }
+}
--- OOStepByStepTest/PersonTest.cs
+++ OOStepByStepTest/PersonTest.cs
@@ -7,12 +7,14 @@ namespace OOStepByStepTest
     using OOStepByStep;
     using Xunit;
 
+    // tests for different kinds of person, including Person, Student and Teacher
     public class PersonTest
     {
         [Fact]
-        public void Should_return_introduction_sentence_when_introduce_oneself_given_a_person()
+        public void Should_return_correct_introduction_sentence_when_introduce_oneself_given_a_person()
         {
             // given
+
             Person person = new Person("Kevin", 28);
 
             // when
@@ -20,6 +22,20 @@ namespace OOStepByStepTest
 
             // then
             Assert.Equal("My name is Kevin. I'm 28 years old.", result);
+        }
+
+        [Fact]
+        public void Should_return_correct_introduction_sentence_when_introduce_oneself_given_a_student()
+        {
+            // given
+
+            Student student = new Student("Kevin", 28);
+
+            // when
+            string result = student.IntroduceOneself();
+
+            // then
+            Assert.Equal("My name is Kevin. I'm 28 years old. I'm a student.", result);
         }
     }
 }
