fc8b5f9603edce8b41bace13a84cb9abac1c459d
Han Xudong <xhan14@slb.com>
2023-11-02 17:23:34 +0800

    feat: Add 'Person' class with 'IntroduceOneself' function.

---
+++ OOStepByStep/Person.cs
@@ -0,0 +1,22 @@
+﻿namespace OOStepByStep
+{
+    using System;
+    using System.Runtime.CompilerServices;
+
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public virtual string IntroduceOneself()
+        {
+            return $"My name is {name}. I'm {age} years old.";
+        }
+    }
+}
---
+++ OOStepByStepTest/PersonTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace OOStepByStepTest
+{
+    using OOStepByStep;
+    using Xunit;
+
+    public class PersonTest
+    {
+        [Fact]
+        public void Should_return_introduction_sentence_when_introduce_oneself_given_a_person()
+        {
+            // given
+            Person person = new Person("Kevin", 28);
+
+            // when
+            string result = person.IntroduceOneself();
+
+            // then
+            Assert.Equal("My name is Kevin. I'm 28 years old.", result);
+        }
+    }
+}
